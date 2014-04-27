f4ee7d4bdf03de862bf30cb3a298b81c01d302c0
Patrick Snape <patricksnape@gmail.com>
2014-04-27 12:51:56 +0000

    Use new theme!

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,26 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
-import os
 
-# Avoid having hard coded paths to these files
-base_path = os.path.dirname(os.path.realpath(__file__))
-THEME = '{}/themes/pelican-responsive/responsive'.format(base_path)
+THEME = u'themes/pure'
 
 AUTHOR = u'Patrick Snape'
-SITENAME = u'patricksnape.github.io'
-SITEURL = 'http://patricksnape.github.io'
-MINI_BIO = u"PhD Candidate at Imperial College London"
-BIO = u"""I am currently working towards
-my PhD at Imperial College London. I'm a member of the
-<strong>I</strong>ntelligent <Strong>B</strong>ehaviour
-<strong>U</strong>nderstanding <strong>G</strong>roup
-(IBUG), focusing on dense reconstruction of facial shape
-from images. In particular, I'm interested in investigating
-the low-rank relationship between images and shape."""
+SITENAME = u'patricksnape'
+SITEURL = u'http://patricksnape.github.io'
+TAGLINE = u'PhD Candidate at Imperial College London'
         
 
-TIMEZONE = 'Europe/London'
+TIMEZONE = u'Europe/London'
 
 DEFAULT_LANG = u'en'
 
@@ -29,14 +19,13 @@ FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
-# Blogroll
-LINKS = ()
+DISPLAY_PAGES_ON_MENU = True
 
 # Social widget
 SOCIAL = (
-    ('Github', 'https://github.com/patricksnape', '&#xe037;'),
-    ('Google Plus', 'https://plus.google.com/+PatrickSnape', '&#xe039;'),
-    ('Twitter', 'https://twitter.com/berecursive', '&#xe086;'),
+    (u'github-square', u'https://github.com/patricksnape'),
+    (u'google-plus-square', u'https://plus.google.com/+PatrickSnape'),
+    (u'twitter-square', u'https://twitter.com/berecursive'),
 )
 
 DEFAULT_PAGINATION = 10
@@ -45,11 +34,14 @@ DEFAULT_PAGINATION = 10
 RELATIVE_URLS = True
 
 
-PLUGIN_PATH = 'plugins'
-PLUGINS = ['liquid_tags.notebook', 'liquid_tags.include_code']
-LIQUID_TAGS_IPYTHON_STYLES = open('_nb_header.html').read().decode('utf-8')
+PLUGIN_PATH = u'plugins'
+PLUGINS = ['gravatar']
 
 DISQUS_SITENAME = u'patricksnape'
 
-PATH = '{}/content'.format(base_path)
-STATIC_PATHS = ['images', 'code', 'notebooks', 'theme/img/avatar.jpg']
+PATH = 'content'
+PROFILE_IMAGE_URL = u'theme/img/avatar.jpg'
+STATIC_PATHS = [u'images']
+
+ARTICLE_URL = u'{date:%Y}/{slug}/'
+ARTICLE_SAVE_AS = u'{date:%Y}/{slug}/index.html'
